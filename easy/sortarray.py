8818c89b163f0a7ff0642e6ff691a80806bd9811
milanpreet91 <kaur.milanpreet7@gmail.com>
2025-08-12 11:18:15 +0530

    Update sortarray.py
    Added heap sort, merge sort, quick sort, insertion sort, bubble sort

--- easy/sortarray.py
+++ easy/sortarray.py
@@ -5,6 +5,7 @@
 # so total work is (num of actions at each level)*(num of levels)
 # TC: nlogn
 
+import sys
 class Solution:
     def merge(self, first: List[int], second: List[int])->List[int]:
         i = 0
@@ -42,11 +43,71 @@ class Solution:
         arr = self.merge(first, second)
         return arr
 
+    ## Quick Sort ##
+    def quicksort(self, arr):
+        if len(arr) <= 1:
+            return arr
+        
+        pivot = arr[-1]
+        left = [x for x in arr[:-1] if x <= pivot]
+        right = [x for x in arr[:-1] if x > pivot]
+        return self.quicksort(left) + [pivot] + self.quicksort(right)
+    #########################
+
+    ### Heap Sort ###
+    def swapmax(self, arr, p, l, r, n):
+        largest = p
+        if r < n and arr[r] > arr[largest]:
+            largest = r
+        if l < n and arr[l] > arr[largest]:
+            largest = l
+        if largest != p: 
+            arr[largest], arr[p] = arr[p], arr[largest]
+            self.heapify(arr, largest, n)
+
+    def swap(self, arr, a, b):
+        return (arr[b], arr[a])
+
+    def heapify(self, arr, i, end):
+        if i >= end:
+            return
+        left = 2*i + 1
+        right = 2*i + 2
+        self.swapmax(arr, i, left, right, end)
+        #if swapped:
+            #print(f"new arr: {arr}")
+            #self.heapify(arr, ind)
+
+    def buildmaxheap(self, arr: List[int])->List[int]:
+        n = len(arr)
+        start = n//2 - 1
+        stop = -1
+        step = -1
+        for i in range(start, stop, step):
+            #print(f"i: {i}")
+            self.heapify(arr, i, n)
+        return arr
+
+    def heapsort(self, arr):
+        arr = self.buildmaxheap(arr)
+        #print(f"arr post max heap built: {arr}")
+        last = len(arr) - 1
+        while last > 0:
+            arr[last], arr[0] = self.swap(arr, last, 0)
+            #print(f"arr post swap: {arr}")
+            self.heapify(arr, 0, last)
+            #print(f"arr post heapify: {arr}")
+            last -= 1
+        return arr
+
     def sortArray(self, nums: List[int]) -> List[int]:
+        sys.setrecursionlimit(20000) 
         n = len(nums)
         ##Merge Sort ## 
-        return self.merge_sort(nums[0:n])
+        #return self.merge_sort(nums[0:n])
         ############
+        #return self.quicksort(nums[0:n])
+        return self.heapsort(nums)
         '''
         ##BubbleSort## O(n^2)
         
